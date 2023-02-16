0d9e9ad12a8af5880be134d21976596844171132
Ziggy Basil <basil99magawi@gmail.com>
2023-02-16 08:50:03 -0800

    fifth challenge

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - program entry point.
+ *
+ * Description: Prints string to stdo using printf.
+ *
+ * Return: 0 = success.
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
