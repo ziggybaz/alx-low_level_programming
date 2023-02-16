ceaffa7d1cb9f8910b38d9994bca19c401de0e6d
Ziggy Basil <basil99magawi@gmail.com>
2023-02-16 10:00:05 -0800

    extras

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ *main - print string to stderr
+ *
+ * Description: function prints output only to stderror
+ *
+ * Return: 1 = error/fail
+ */
+
+int main(void)
+{
+
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar , 2015-10-19");
+	return(1);
+}
