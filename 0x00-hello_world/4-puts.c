76dfe889474632fb6801c48c51980563570093e7
Ziggy Basil <basil99magawi@gmail.com>
2023-02-16 08:13:02 -0800

    using the puts function.

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - the function returns a character of strings.
+ *
+ * Description: The function receives strings as an argument an displays it to the stdo using the 'puts' function.
+ *
+ * Return: It returns 0 which means success
+ */
+int main(void)
+{
+	char str1[]="\"Programming is like building a multilingual puzzle";
+	puts(str1);
+			
+	returns(0);
+}
