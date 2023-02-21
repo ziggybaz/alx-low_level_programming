948b488d9b77b54fb699682b65d21e58ac7d0fbd
ziggybaz <77249316+ziggybaz@users.noreply.github.com>
2023-02-21 22:28:06 +0300

    Update 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,5 +6,33 @@
  */
 void times_table(void)
 {
-	_putchar(90);
+	int mult;
+	int num;
+	int val;
+	for (mult = 0 ; mult <= 9 ; mult++)
+	{
+	for (num = 0 ; num <= 9 ; num++)
+		{
+		val = mult * num;
+		if (num == 0)
+		{
+		_putchar(val + '0');
+		}
+		else if (num > 0 && val < 10)
+		{
+		_putchar(',');
+		_putchar(' ');
+		_putchar(' ');
+		_putchar(val + '0');
+		}
+		else
+		{
+		_putchar(',');
+		_putchar(' ');
+		_putchar((val / 10) + '0');
+		_putchar((val % 10) + '0');
+		}
+	}
+	_putchar('\n');
+}
 }
