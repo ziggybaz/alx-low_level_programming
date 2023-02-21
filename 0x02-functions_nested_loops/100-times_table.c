62441e712815eaa07affce8e066c5a8fe0ab0c4e
ziggybaz <77249316+ziggybaz@users.noreply.github.com>
2023-02-22 02:52:43 +0300

    Update 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -10,5 +10,34 @@
  */
 void print_times_table(int n)
 {
-	_putchar();
+	int x, y, num;
+
+	if (n <= 15 && n >= 0)
+	{
+		for (num = 0; num <= n; ++num)
+		{
+			_putchar(48);
+			for (y = 1; y <= n; ++y)
+			{
+				_putchar(',');
+				_putchar(' ');
+
+				x = num * y;
+
+				if (x <= 9)
+					_putchar(' ');
+				if (x <= 99)
+					_putchar(' ');
+
+				if (x >= 100)
+				{
+					_putchar((x / 100) + 48);
+					_putchar((x / 10) % 10 + 48);
+				} else if (x <= 99 && x >= 10)
+					_putchar((prod / 10) + 48);
+				_putchar((x % 10) + 48);
+			}
+			_putchar('\n');
+		}
+	}
 }
