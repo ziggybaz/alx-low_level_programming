0bfdd36954fa36516e899e2ff6a70d834cfb029b
Ziggy Basil <basil99magawi@gmail.com>
2023-02-21 11:54:27 -0800

    ok

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_to_98 - print natural numbers
+ *
+ * Description: print natural numbers between 0-98
+ *
+ * @n: integer type-checker
+ *
+ * Return: numbers
+ */
+void print_to_98(int n)
+{
+	return (n + 20);
+}
