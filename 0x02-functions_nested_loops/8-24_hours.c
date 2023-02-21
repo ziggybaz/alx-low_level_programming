c05e02059b7d9a951da20d10c40694085ca7d23f
Ziggy Basil <basil99magawi@gmail.com>
2023-02-21 11:17:52 -0800

    ok

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * jack_bauer - prints minutes of day
+ *
+ * Description: prints every minute of the day
+ *
+ * Return: minute of the day
+ */
+void jack_bauer(void)
+{
+	_putchar(0);
+}
