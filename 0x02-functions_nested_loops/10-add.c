dc4d4c8fe8ab6cb790be068af09dfb95431b867b
Ziggy Basil <basil99magawi@gmail.com>
2023-02-21 11:42:08 -0800

    ok

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * add - add 2 integers
+ *
+ * Description: add 2 integers
+ *
+ * @x: integer type-checker
+ *
+ * @y: integer type-checker
+ *
+ * Return: sum
+ */
+int add(int x, int y)
+{
+	return (x + y);
+}
