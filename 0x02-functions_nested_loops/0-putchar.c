075fff113806e26e7eed556eca320ebcc550e7ae
ziggybaz <77249316+ziggybaz@users.noreply.github.com>
2023-02-21 18:57:59 +0300

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -10,11 +10,12 @@
 
 int main(void)
 {
-	char myString[9] = "_putchar";
+	char myString[] = "_putchar";
 	int c;
 
-	for (c = 0; c < 9; ++c)
+	for (c = 0; c < 8; ++c)
 		_putchar(myString[c]);
-
+	_putchar('\n');
+	
 	return (0);
 }
