7b09f8afc57e7d247c9291d3cb557028841dc6ce
ziggybaz <77249316+ziggybaz@users.noreply.github.com>
2023-02-21 22:19:10 +0300

    Update 8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,5 +8,18 @@
  */
 void jack_bauer(void)
 {
-	_putchar(0);
+	int min, hr;
+
+	for (hr = 0; hr <= 23; ++hr)
+	{
+		for (min = 0; min <= 59; ++min)
+		{
+			_putchar((hr / 10) + 48);
+			_putchar((hr % 10) + 48);
+			_putchar(':');
+			_putchar((min / 10) + 48);
+			_putchar((min % 10) + 48);
+			_putchar('\n');
+		}
+	}
 }
