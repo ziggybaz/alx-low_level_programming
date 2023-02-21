3a76149060fe48b6fbd14b367b8d1bef5ef40850
Ziggy Basil <basil99magawi@gmail.com>
2023-02-21 15:50:24 -0800

    extras

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_times_table - prints the n times table, starting with 0
+ *
+ * Description: function that prints the n times table, starting with 0.
+ *
+ * @n: integer type-checker
+ *
+ * Return: n timestable
+ */
+void print_times_table(int n)
+{
+	_putchar();
+}
