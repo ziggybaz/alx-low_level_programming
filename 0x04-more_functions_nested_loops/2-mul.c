a03b27b640956767e550cbbf639f7c0c707b293e
Ziggy Basil <basil99magawi@gmail.com>
2023-02-23 17:13:19 -0800

    k

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/*
+ * mul - multiply two integers
+ *
+ * @a: typechecker
+ *
+ * @b: typechecker
+ *
+ * Description: function that multiplies two integers
+ *
+ * Return: answer
+ */
+int mul(int a, int b)
+{
+	int ans;
+	ans = a * b;
+
+	return (ans);
+}
